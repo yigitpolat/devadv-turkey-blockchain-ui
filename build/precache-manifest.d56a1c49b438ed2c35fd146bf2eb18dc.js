54fbcf44f05e63160692637c0046082498793c74
Kemal Aydın <aydinkemal@yandex.com.tr>
2019-09-03 16:07:33 +0300

    First Commit

---
+++ build/precache-manifest.d56a1c49b438ed2c35fd146bf2eb18dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ca56cd7328d1e43bce0b157f8e7b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d66d251ead702a4d436f",
+    "url": "/static/css/main.61791fb9.chunk.css"
+  },
+  {
+    "revision": "b8220d68e88deb9675d4",
+    "url": "/static/js/2.f8ed8c80.chunk.js"
+  },
+  {
+    "revision": "d66d251ead702a4d436f",
+    "url": "/static/js/main.fca82c29.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
